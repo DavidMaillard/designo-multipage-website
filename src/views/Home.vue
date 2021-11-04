a931e292797f8f6338796eea7af6cfc8e4ee0147
David Maillard <maillard.david23@gmail.com>
2021-11-04 17:09:56 -0400

    :sparkles: home hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="home-page">
-    Home
+    <section class="home-hero">
+      <section class="home-hero-container">
+        <h1 class="hero-heading">Award-winning custom designs and digital branding solutions</h1>
+        <p class="hero-text">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+        <router-link to="/about" class="hero-button">Learn more</router-link>
+      </section>
+    </section>
   </section>
 </template>
 
