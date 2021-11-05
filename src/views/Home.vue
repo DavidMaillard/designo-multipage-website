a1712ae9fd9fa55e75ef562e355bcedb783bbc4f
David Maillard <maillard.david23@gmail.com>
2021-11-05 12:10:41 -0400

    :sparkles: home arguments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,6 +27,38 @@
         </div>
       </section>
     </section>
+    <!-- Home Arguments -->
+    <section class="home-arguments">
+      <div class="home-arguments-container">
+        <article class="home-argument">
+          <div class="argument-image-container">
+            <img src="/assets/home/desktop/illustration-passionate.svg" alt="Passionate" class="argument-image">
+          </div>
+          <div class="argument-info">
+            <p class="argument-title">PASSIONATE</p>
+            <p class="argument-text">Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.</p>
+          </div>
+        </article>
+        <article class="home-argument">
+          <div class="argument-image-container">
+            <img src="/assets/home/desktop/illustration-resourceful.svg" alt="Passionate" class="argument-image">
+          </div>
+          <div class="argument-info">
+            <p class="argument-title">RESOURCEFUL</p>
+            <p class="argument-text">Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.</p>
+          </div>
+        </article>
+        <article class="home-argument">
+          <div class="argument-image-container">
+            <img src="/assets/home/desktop/illustration-friendly.svg" alt="Passionate" class="argument-image">
+          </div>
+          <div class="argument-info">
+            <p class="argument-title">FRIENDLY</p>
+            <p class="argument-text"> We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.</p>
+          </div>
+        </article>
+      </div>
+    </section>
   </section>
 </template>
 
