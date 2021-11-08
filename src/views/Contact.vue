e6e30ff6abbd48b031103c4ed96f9526f750e0f2
David Maillard <maillard.david23@gmail.com>
2021-11-08 14:58:14 -0500

    :sparkles: prepare last sections

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,32 @@
 <template>
   <section class="contact-page">
-    Contact
+    <!-- Contact Form -->
+    <!-- Locations -->
+    <div class="locations">
+      <div class="locations-container">
+        <article class="location-item">
+          <div class="location-illustration-container">
+            <img src="/assets/shared/desktop/illustration-canada.svg" alt="Canada" class="location-illustration">
+          </div>
+          <p class="location-title">Canada</p>
+          <router-link to="/locations" class="location-link">See location</router-link>
+        </article>
+        <article class="location-item">
+          <div class="location-illustration-container">
+            <img src="/assets/shared/desktop/illustration-australia.svg" alt="Australia" class="location-illustration">
+          </div>
+          <p class="location-title">Australia</p>
+          <router-link to="/locations" class="location-link">See location</router-link>
+        </article>
+        <article class="location-item">
+          <div class="location-illustration-container">
+            <img src="/assets/shared/desktop/illustration-united-kingdom.svg" alt="United Kingdom" class="location-illustration">
+          </div>
+          <p class="location-title">United Kingdom</p>
+          <router-link to="/locations" class="location-link">See location</router-link>
+        </article>
+      </div>
+    </div>
   </section>
 </template>
 
--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="locations-page">
-    Locations
+    <!-- Locations -->
   </section>
 </template>
 
